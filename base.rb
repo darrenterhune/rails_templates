01a8296823ae0431ee6b26d0d93fc0fb9d2274f1
Darren Terhune <darren@headfirstproductions.ca>
2010-12-03 19:18:40 -0800

    updated readme, added cssgrid layout based on cssgrid.net

--- base.rb
+++ base.rb
@@ -1,9 +1,98 @@
 puts "Setting up your app..."
 
 run "mv README README.rdoc"
-run "echo h1. Welcome > README.rdoc"
+file "README.rdoc", <<-CODE
+== Setup
+
+Make sure mysql is running... and you've configured <tt>config/database.yml</tt>
+
+ $ sudo rake gems:install
+ $ rake db:create
+ $ rake db:migrate
+ 
+ $ cd /path/to/app
+ $ script/server
+
+== Requirements
+
+ 1. Ruby 1.8.6+
+ 2. Rails 2.3+
+ 3. Mysql
+ 4. Some development know how
+ 5. Debugging skill if anything goes wrong
+
+== Deploying
+
+We're using capistrano deployment functionality of the excellent
+<tt>capistrano</tt> gem to allow you to deploy to your production server. 
+If you don't already have this gem installed, please do so by running:
+
+ $ sudo gem install capistrano
+
+To deploy the applications... yes you don't need sftp or ftp clients for this, do:
+
+ $ cap deploy
+ 
+On every deployment, <tt>cap deploy</tt> it will upload all modified files
+to the production server, remove cache and then restart passenger. That's it? 
+Huh? Ya that's the Ruby way. To restart Passenger aka your app:
+
+ $ cap deploy:restart
+ 
+To empty cache... if there's any cached files:
+
+ $ cap deploy:remove_cache
+
+== Generating Models/Controllers/Mailers
+
+To generate new code you'll want to do something like:
+
+ $ rake rspec_controller name_of_controller
+ $ rake rspec_model name_of_model
+
+== Backups
+
+There is a bash/shell script that runs to backup the database once a week.
+Example cron would be:
+
+ * 0 2 * * 1 /home/deezbg/backups/mysql.sh
+
+== Testing
+
+No tests have been written, however all files and spec framework are there.
+To write test open up spec/ files and write them.
+
+== Other
+
+There is more readme and file in doc/ if you put any files in there and are
+using git or any other version control make sure you don't add that dir or
+at least doc/README_FOR_APP as it contains some gooders.
+
+CODE
 run "rm -rf public/index.html"
 run "touch config/config.yml"
+
+puts "Removing prototype... we like jQuery better"
+run "rm -rf public/javascripts/*.js"
+file "public/javascripts/app.js", <<-CODE
+//Add your applications js here
+CODE
+
+file "app/views/layouts/application.html.erb", <<-CODE
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+	<meta name="description" content=""/>
+	<meta name="keywords" content=""/>
+	<title></title>
+</head>
+<body>
+  <div class="container">
+  </div>
+</body>
+</html>
+CODE
 initializer "load_config.rb", <<-CODE
 AppConfig = YAML.load_file(File.join(RAILS_ROOT, 'config', 'config.yml'))
 CODE
@@ -66,49 +155,57 @@ run "capify ."
 
 if yes?("Do you want to setup general deploy configs? (Dreamhost only)")
   
-  file "config/deploy.rb", <<-END
-  set :user, '_your-user_'
-  set :domain, '_your-server_.dreamhost.com'
-  set :project, '_your-project_'
-  set :application, '_your-application_'
-  set :deploy_to, "/home/#{user}/#{application}"
-  set :deploy_via, :copy
-  set :use_sudo, false
-  set :repository, "."
-  set :scm, :none
+user = ask("What is the user this app will be deployed to?")
+servername = ask("What is the name of the server? eg: bluebird.dreamhost.com")
+project = @root.split('/').last
+app = ask("What's the url for this domain?")
+localuser = ask("What's the name of the localuser on your computer? This will be used to locate your .ssh key for deployment.")
 
-  role :web, domain
-  role :app, domain
-  role :db,  domain, :primary => true
-  ssh_options[:keys] = %w(/Users/darrenterhune/.ssh/id_rsa)
+file "config/deploy.rb", <<-CODE
+set :user, "#{user}"
+set :domain, "#{servername}"
+set :project, "#{project}"
+set :application, "#{app}"
+set :deploy_to, "/home/#{user}/#{app}"
+set :deploy_via, :copy
+set :use_sudo, false
+set :repository, "."
+set :scm, :none
 
-  namespace :deploy do
+role :web, domain
+role :app, domain
+role :db,  domain, :primary => true
+ssh_options[:keys] = %w(/Users/#{localuser}/.ssh/id_rsa)
 
-    desc "Tell Passenger to restart."
-    task :restart, :roles => :web do
-      run "cd #{deploy_to}/current && RAILS_ENV=production rake db:migrate"
-      run "touch #{deploy_to}/current/tmp/restart.txt" 
-    end
+namespace :deploy do
 
-    desc "Do nothing on startup so we don't get a script/spin error."
-    task :start do
-      puts "You may need to restart Apache."
-    end
-
-    desc "Remove all cached files from server"
-    task :remove_cache, :roles => :web do
-      run "rm -rf #{deploy_to}/current/public/sitemap.xml"
-      run "rm -rf #{deploy_to}/current/public/index.html"
-      puts "cache removed!"
-    end
-
+  desc "Tell Passenger to restart."
+  task :restart, :roles => :web do
+    run "cd deployto/current && RAILS_ENV=production rake db:migrate"
+    run "touch deployto/current/tmp/restart.txt" 
   end
 
-  after "deploy", "deploy:cleanup"
-  after "deploy", "deploy:remove_cache"
-  END
+  desc "Do nothing on startup so we don't get a script/spin error."
+  task :start do
+    puts "You may need to restart Apache."
+  end
+
+  desc "Remove all cached files from server"
+  task :remove_cache, :roles => :web do
+    run "rm -rf deployto/current/public/sitemap.xml"
+    run "rm -rf deployto/current/public/index.html"
+    puts "cache removed!"
+  end
+
+end
+
+after "deploy", "deploy:cleanup"
+after "deploy", "deploy:remove_cache"
+CODE
   
 end
+
+puts "Giting init..."
 
 git :init
 
@@ -122,18 +219,33 @@ tmp/**/*
 config/database.yml
 config/config.yml
 config/initializers/session_store.rb
+config/initializers/site_keys.rb
 config/initializers/cookie_verification_secret.rb
 db/*.sqlite3
 CODE
+
+puts "Committed initial commit..."
 
 git :add => ".", :commit => "-m 'initial commit'"
 
 if yes?("Do you want restful-authentication?")
   
   plugin "restul-authentication", :git => "git://github.com/technoweenie/restful-authentication.git"
+  
+  if yes?("Are you running Rails 2.3.8+?")
+    puts "Moving restful-authentication tasks to lib..."
+    run "mv vendor/plugins/restful-authentication/tasks/auth.rake* lib/tasks/auth.rake"
+    run "rm -rf vendor/plugins/restful-authentication/tasks"
+  end
+  
   generate :authenticated, "admin sessions --rspec"
   
-  rake "db:migrate"
+  if yes?("Do you wanna create the database and run the migrations?")
+    rake "db:create"
+    rake "db:migrate"
+  end
   
   git :add => ".", :commit => "-m 'added authentication'"
 end
+
+puts "Base template has been applied..."
--- cssgrid.rb
+++ cssgrid.rb
@@ -1,44 +1,73 @@
-load_template "http://github.com/darrenterhune/rails_templates/raw/master/base.rb"
-
-if yes?("How bout that awesome 1140 grid layout?")
-  puts "Fetching css grid version 1.6..."
-  run "wget http://download.cssgrid.net/1140_CssGrid_1.6.zip"
-  puts "Un packing the files..."
-  run "unzip 1140_CssGrid_1.6.zip"
-  run "mv css/* "
-  puts "House cleaning..."
-  
-  puts "Generating default layout for cssgrid..."
-  file "app/views/layouts/application.html.erb", <<-END
-  <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-  <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
-  <head>
-  	<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
-  	<meta name="description" content="<%= @description || "" %>"/>
-  	<meta name="keywords" content="<%= @keywords || "" %>"/>
-  	<title><%= @page_title || "" %></title>
-  </head>
-  <body>
-    <div class="container">
-      <div class="row">
-        <div class="threecol">
-          <p>Column 1</p>
-        </div>
-        <div class="threecol">
-          <p>Column 2</p>
-        </div>
-        <div class="threecol">
-          <p>Column 3</p>
-        </div>
-        <div class="threecol last">
-          <p>Column 4</p>
-        </div>
-      </div>
-    </div>
-  </body>
-  </html>
-  END
-  
+if yes?("Do you want to use the base.rb template first?")
+  load_template "/Users/darrenterhune/Code/rails_templates/base.rb"
 end
 
-puts "Finished! Enjoy..."
+if yes?("How bout that awesome 1140 grid layout?")
+puts "Fetching css grid version 1.6..."
+run "wget http://download.cssgrid.net/1140_CssGrid_1.6.zip"
+puts "Un packing the files..."
+run "unzip 1140_CssGrid_1.6.zip"
+run "mv css/* public/stylesheets"
+puts "House cleaning..."
+run "rm -rf __MACOSX"
+run "rm -rf 1140_CssGrid_1.6"
+run "rm -rf css"
+run "rm index.html"
+run "rm 1140_CssGrid_1.6.zip"
+
+puts "Generating default layout for cssgrid..."
+author = ask("What do you want the meta author tag to be?")
+title = ask("What do you want the title to be?")
+keywords = ask("What do you want the keywords to be?")
+description = ask("What do you want the description to be?")
+
+file "app/views/layouts/application.html.erb", <<-END
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"/>
+  <meta name="author" content="#{author}"/>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+	<meta name="description" content="#{description}"/>
+	<meta name="keywords" content="#{keywords}"/>
+	<title>#{title}</title>
+  <link rel="apple-touch-icon" href="apple-touch-icon-iphone.png" />
+  <link rel="apple-touch-icon" sizes="72x72" href="apple-touch-icon-ipad.png" />
+  <link rel="apple-touch-icon" sizes="114x114" href="apple-touch-icon-iphone4.png" />
+  <link rel="stylesheet" href="/stylesheets/1140.css" type="text/css" media="screen"/>
+  <!--[if lte IE 9]><link rel="stylesheet" href="/stylesheets/ie.css" type="text/css" media="screen"/><![endif]-->	
+  <link rel="stylesheet" href="/stylesheets/typeimg.css" type="text/css" media="screen"/>
+  <link rel="stylesheet" href="/stylesheets/smallerscreen.css" media="only screen and (max-width: 1023px)"/>
+  <link rel="stylesheet" href="/stylesheets/mobile.css" media="handheld, only screen and (max-width: 767px)"/>
+  <link rel="stylesheet" href="/stylesheets/layout.css" type="text/css" media="screen"/>
+  <script language="javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.3.1/jquery.min.js" type="text/javascript"></script>
+</head>
+<body>
+  <div class="container">
+    <div class="row">
+      <div class="threecol">
+        <p>Column 1</p>
+      </div>
+      <div class="threecol">
+        <p>Column 2</p>
+      </div>
+      <div class="threecol">
+        <p>Column 3</p>
+      </div>
+      <div class="threecol last">
+        <p>Column 4</p>
+      </div>
+    </div>
+  </div>
+</body>
+</html>
+END
+end
+
+puts "You will want to add icons for ipad, iphone, itouch and browsers:"
+puts "57x57 - public/apple-touch-icon-iphone.png"
+puts "72x72 - public/apple-touch-icon-ipad.png"
+puts "114x114 - public/apple-touch-icon-iphone4.png"
+
+puts "CSSGrid template applied..."
